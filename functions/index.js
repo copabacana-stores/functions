e0b1988651952e906968385fbc6c541ab32ea774
maothinks <maothinks@gmail.com>
2020-02-25 19:40:23 -0500

    Added new function generateThumbnail

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp()
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
+    const fileBucket = object.bucket; // The Storage bucket that contains the file.
+    const filePath = object.name; // File path in the bucket.
+    const contentType = object.contentType; // File content type.
+    const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
+
+    if (!contentType.startsWith('image/')) {
+        return console.log('This is not an image.');
+    }
+
+    const fileName = path.basename(filePath);
+
+    if (fileName.startsWith('thumb_')) {
+        return console.log('Already a Thumbnail.');
+    }
+
+    // Download file from bucket.
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    const metadata = {
+        contentType: contentType,
+    };
+    await bucket.file(filePath).download({ destination: tempFilePath });
+    console.log('Image downloaded locally to', tempFilePath);
+    // Generate a thumbnail using ImageMagick.
+    await spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+    console.log('Thumbnail created at', tempFilePath);
+    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+    const thumbFileName = `thumb_${fileName}`;
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    // Uploading the thumbnail.
+    await bucket.upload(tempFilePath, {
+        destination: thumbFilePath,
+        metadata: metadata,
+    });
+    // Once the thumbnail has been uploaded delete the local file to free up disk space.
+    return fs.unlinkSync(tempFilePath);
 });
